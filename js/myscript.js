0fe87e9408e8d996a051261bacf02d7a66338f3d
Vale <oirelav95@gmail.com>
2022-11-14 15:03:37 +0100

    creo array con lista della spesa

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina gli elementi della lista 
+// individualmente con un ciclo for che poi commentate, e poi fate lo stesso con il while (che lasciate attivo).
+// Bonus;
+// l’utente può inserire al volo elementi alla lista.
+
+const listaSpesa = ["Pane", "Pasta", "Insalata", "Carne"];
